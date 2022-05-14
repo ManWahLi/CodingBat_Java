0c3025770c6653a74bad4c2c7189a2062fbdfe6a
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-14 17:55:24 -0500

    Update LoneTeen.java

--- WarmUp1/LoneTeen.java
+++ WarmUp1/LoneTeen.java
@@ -11,5 +11,5 @@ loneTeen(13, 13) → false
 public boolean loneTeen(int a, int b) 
 {
   return ((a >= 13 && a <= 19) || (b >=13 && b <= 19)) &&
-         !((a >= 13 && a <= 19) && (b >=13 && b <= 19));
+         !(a >= 13 && a <= 19 && b >= 13 && b <= 19);
 }
