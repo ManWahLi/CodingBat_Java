1858b9f91fa41538fc47fbfa0dfc94bf4cadb355
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-17 15:01:32 -0500

    Create DoubleX.java

---
+++ WarmUp2_Medium/DoubleX.java
@@ -0,0 +1,16 @@
+/*
+Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+doubleX("axxbb") → true
+doubleX("axaxax") → false
+doubleX("xxxxx") → true
+*/
+
+public boolean doubleX(String str) 
+{
+  int i = str.indexOf("x");
+  
+  String x = i != -1 ? str.substring(i) : "";
+  
+  return x.startsWith("xx");
+}
