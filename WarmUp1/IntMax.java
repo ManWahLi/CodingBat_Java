5dce0e663a6f34ec179b0452b18985df5d13d844
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-19 13:46:34 -0500

    Update IntMax.java

--- WarmUp1/IntMax.java
+++ WarmUp1/IntMax.java
@@ -4,6 +4,8 @@ Given three int values, a b c, return the largest.
 intMax(1, 2, 3) → 3
 intMax(1, 3, 2) → 3
 intMax(3, 2, 1) → 3
+
+@author Man Wah Li [limanwah0924@gmail.com]
 */
 
 public int intMax(int a, int b, int c) 
