3c4579654735d7a019a15d6338ddf82bc2d58add
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-19 13:50:39 -0500

    Update StringE.java

--- WarmUp1/StringE.java
+++ WarmUp1/StringE.java
@@ -4,6 +4,8 @@ Return true if the given string contains between 1 and 3 'e' chars.
 stringE("Hello") → true
 stringE("Heelle") → true
 stringE("Heelele") → false
+
+@author Man Wah Li [limanwah0924@gmail.com]
 */
 
 public boolean stringE(String str) 
