ca7b45d6603bc92e2f4cec92a9d8d4eb37083ccf
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-19 13:45:51 -0500

    Update IcyHot.java

--- WarmUp1/IcyHot.java
+++ WarmUp1/IcyHot.java
@@ -4,6 +4,8 @@ Given two temperatures, return true if one is less than 0 and the other is great
 icyHot(120, -1) → true
 icyHot(-1, 120) → true
 icyHot(2, 120) → false
+
+@author Man Wah Li [limanwah0924@gmail.com]
 */
 
 public boolean icyHot(int temp1, int temp2) 
