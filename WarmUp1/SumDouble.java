c13d8c9cc75476f8b6af6c6125d0da2993505449
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-19 13:50:50 -0500

    Update SumDouble.java

--- WarmUp1/SumDouble.java
+++ WarmUp1/SumDouble.java
@@ -4,6 +4,8 @@ Given two int values, return their sum. Unless the two values are the same, then
 sumDouble(1, 2) → 3
 sumDouble(3, 2) → 5
 sumDouble(2, 2) → 8
+
+@author Man Wah Li [limanwah0924@gmail.com]
 */
 
 public int sumDouble(int a, int b) 
