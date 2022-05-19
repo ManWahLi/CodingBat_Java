c75d78ffadb42aee1bb5a424b69c30e133310373
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-19 13:48:18 -0500

    Update MixStart.java

--- WarmUp1/MixStart.java
+++ WarmUp1/MixStart.java
@@ -4,6 +4,8 @@ Return true if the given string begins with "mix", except the 'm' can be anythin
 mixStart("mix snacks") → true
 mixStart("pix snacks") → true
 mixStart("piz snacks") → false
+
+@author Man Wah Li [limanwah0924@gmail.com]
 */
 
 public boolean mixStart(String str) 
