ff6fab3b879e1fc8be508c72134176e3ec2ac960
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-26 09:12:34 -0500

    Create Array667.java

---
+++ WarmUp2_Medium/Array667.java
@@ -0,0 +1,23 @@
+/*
+Given an array of ints, return the number of times that two 6's are next to each other in the array. 
+Also count instances where the second "6" is actually a 7.
+
+array667([6, 6, 2]) → 1
+array667([6, 6, 2, 6]) → 1
+array667([6, 7, 2, 6]) → 1
+
+@author Man Wah Li [limanwah0924@gmail.com]
+*/
+
+public int array667(int[] nums) 
+{
+  int count = 0;
+  
+  for(int i = 0; i + 1 < nums.length; i++)
+  {
+    count = nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7) ?
+            ++count : count;
+  }
+
+  return count;
+}
