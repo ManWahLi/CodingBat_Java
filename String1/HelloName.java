e99d3bd2682fe81516ee81b1d55c8d0c1c2ec1e5
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-05-29 08:21:49 -0500

    Create HelloName.java

---
+++ String1/HelloName.java
@@ -0,0 +1,14 @@
+/*
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+
+@author Man Wah Li [limanwah0924@gmail.com]
+*/
+
+public String helloName(String name) 
+{
+  return "Hello " + name + "!";
+}
