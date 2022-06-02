bc9aa2d439e7f25ec8e1c068c715b37ba71b8d5d
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-06-02 16:00:50 -0500

    Create NonStart.java

---
+++ String1/NonStart.java
@@ -0,0 +1,15 @@
+/*
+Given 2 strings, return their concatenation, except omit the first char of each. 
+The strings will be at least length 1.
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+
+@author Man Wah Li [limanwah0924@gmail.com]
+*/
+
+public String nonStart(String a, String b) 
+{
+  return a.substring(1, a.length()) + b.substring(1, b.length());
+}
