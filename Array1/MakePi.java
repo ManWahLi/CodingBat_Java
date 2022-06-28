94089bff6c8c01325d00b46d700c4992617bde9a
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-06-28 09:12:01 -0500

    Create MakePi.java

---
+++ Array1/MakePi.java
@@ -0,0 +1,14 @@
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+
+@author Man Wah Li [limanwah0924@gmail.com]
+*/
+
+public int[] makePi() 
+{
+  int[] pi = {3, 1, 4};
+  
+  return pi;
+}
